e72fc507cbf554afb188b9db4f483e6804ea7224
Oyin <oyindev@gmail.com>
2025-12-04 17:50:46 -0500

    Update API URL to run on cloud

--- server/index.js
+++ server/index.js
@@ -61,5 +61,5 @@ app.get('/api/tasks', async (req, res) => {
 });
 
 //start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
